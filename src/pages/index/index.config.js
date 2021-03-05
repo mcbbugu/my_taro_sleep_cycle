d433d56e18a65b9792f4c20f01a939459149c33a
15268343252 <1443786299@qq.com>
2021-03-05 23:50:34 +0800

    Initial commit

---
+++ src/pages/index/index.config.js
@@ -0,0 +1,3 @@
+export default {
+  navigationBarTitleText: '睡眠周期实验'
+}
---
+++ src/pages/index/index.jsx
@@ -0,0 +1,55 @@
+import {Component} from 'react'
+import {View, Text, OpenData} from '@tarojs/components'
+import {AtButton} from 'taro-ui'
+import Taro from '@tarojs/taro'
+import "taro-ui/dist/style/components/button.scss" // 按需引入
+import './index.css'
+
+export default class Index extends Component {
+
+  componentWillMount() {
+  }
+
+  componentDidMount() {
+
+  }
+
+  componentWillUnmount() {
+  }
+
+  componentDidShow() {
+  }
+
+  componentDidHide() {
+  }
+
+  login(e) {
+    console.log(e.detail.userInfo, "go")
+    Taro.login({
+      success: function (res) {
+        if (res.code) {
+          //发起网络请求
+          Taro.request({
+            url: 'http://127.0.0.1:8081/login',
+            data: {
+              code: res.code
+            }
+          }).then((data) => {
+            console.log(data.data)
+          })
+        } else {
+          console.log('登录失败！' + res.errMsg)
+        }
+      }
+    })
+  }
+
+  render() {
+    return (
+      <View className='index'>
+        <AtButton type='primary' openType="getUserInfo" onGetUserInfo={this.login}>登录</AtButton>
+        <OpenData type='userAvatarUrl'/>
+      </View>
+    )
+  }
+}
